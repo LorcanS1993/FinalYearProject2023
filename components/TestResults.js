bbf0f3cef6ac596cb0fa3306ece970b586c2b9b9
Lorcan1993 <G00289865@atu.ie>
2023-04-26 23:36:29 +0100

    Code to display BAC results to mobile app

--- components/TestResults.js
+++ components/TestResults.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { firebase } from '@react-native-firebase/database';
+import { ref, onValue, get } from 'firebase/database';
+import { database } from "../config/firebase";
 
 const TestResults = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState(0);
 
   useEffect(() => {
-    const dbRef = firebase.database().ref('/AlcoholTest/mgL');
-    dbRef.on('value', snapshot => {
-      setData(snapshot.val());
+    const dbRef = ref(database, '/AlcoholTest/mgL');
+    get(dbRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        setData(snapshot.val());
+      }
+    }).catch((error) => {
+      console.error(error);
     });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{data.result}</Text>
+      <Text style={styles.text}>{data}</Text>
     </View>
   );
 };
@@ -32,3 +37,4 @@ const styles = StyleSheet.create({
 });
 
 export default TestResults;
+
