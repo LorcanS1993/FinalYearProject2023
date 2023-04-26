8fc9fd1a403f7087b6527dc83034fff59b59d014
Lorcan1993 <G00289865@atu.ie>
2023-04-26 23:36:57 +0100

    Configured Real Time Database to firebase.js file

--- config/firebase.js
+++ config/firebase.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import Constants from "expo-constants";
 import { getReactNativePersistence } from "firebase/auth/react-native";
@@ -6,9 +5,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,11 +18,26 @@ const firebaseConfig = {
   appId: Constants.manifest?.extra?.firebaseAppId,
 };
 
+const firebaseConfig2 = {
+  apiKey: "AIzaSyAz7X7ptzblcrEoLICDXHsYe3D5N19sW60",
+  authDomain: "fir-connect1993.firebaseapp.com",
+  databaseURL: "https://fir-connect1993-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "fir-connect1993",
+  storageBucket: "fir-connect1993.appspot.com",
+  messagingSenderId: "46357520434",
+  appId: "1:46357520434:web:90d7d085da52e51e71dc2b",
+  measurementId: "G-DKSYQ43XHH"
+};
+
 const app = initializeApp(firebaseConfig);
+const app2 = initializeApp(firebaseConfig2, "secondary");
+
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
-const db = getFirestore();
-const storage = getStorage();
 
-export { auth, db, storage };
+const db = getFirestore(app);
+const storage = getStorage(app);
+const database = getDatabase(app2);
+
+export { auth, db, storage, database };
