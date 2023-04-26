273acebd50d342c88c51a17f3ea5b5070c8ac1e1
Lorcan1993 <G00289865@atu.ie>
2023-04-26 23:52:19 +0100

    Redesign of TestResults.js

--- components/TestResults.js
+++ components/TestResults.js
@@ -1,25 +1,51 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Animated, Easing } from 'react-native';
 import { ref, onValue, get } from 'firebase/database';
 import { database } from "../config/firebase";
 
 const TestResults = () => {
   const [data, setData] = useState(0);
+  const [fadeAnim] = useState(new Animated.Value(0));
 
   useEffect(() => {
     const dbRef = ref(database, '/AlcoholTest/mgL');
     get(dbRef).then((snapshot) => {
       if (snapshot.exists()) {
         setData(snapshot.val());
+        animate();
       }
     }).catch((error) => {
       console.error(error);
     });
   }, []);
 
+  const animate = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      easing: Easing.ease,
+      useNativeDriver: true,
+    }).start();
+  }
+
+  const refreshData = () => {
+    const dbRef = ref(database, '/AlcoholTest/mgL');
+    get(dbRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        setData(snapshot.val());
+        animate();
+      }
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{data}</Text>
+      <Text style={styles.title}>YOUR BAC LEVEL IS</Text>
+      <Animated.View style={{opacity: fadeAnim}}>
+        <Text style={styles.text}>{data}</Text>
+      </Animated.View>
     </View>
   );
 };
@@ -29,12 +55,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#azure',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'red',
+    marginBottom: 20,
   },
   text: {
-    fontSize: 24,
+    fontSize: 48,
     fontWeight: 'bold',
+    color: 'red',
+    textAlign: 'center',
   },
 });
 
 export default TestResults;
-
